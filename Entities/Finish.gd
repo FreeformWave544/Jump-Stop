db7ac353e848c9ad5bb4901f995170726bbec787
FreeformWave544 <165945352+FreeformWave544@users.noreply.github.com>
2026-09-12 20:24:26 +0100

    Quick Bug Patch

--- Entities/Finish.gd
+++ Entities/Finish.gd
@@ -7,4 +7,4 @@ func _on_body_entered(body: Node2D) -> void:
 		body.find_child("Sprite2D").texture.gradient.set_color(0, Color(0.349, 1.0, 0.0, 1.0))
 		body.find_child("Sprite2D").texture.gradient.set_color(1, Color(0.502, 1.0, 0.0, 0.0))
 		await get_tree().create_timer(0.5).timeout
-		if is_inside_tree(): get_tree().change_scene_to_packed(next_level if (next_level and ResourceLoader.exists(next_level.resource_path)) else "res://Scenes/main_menu.tscn")
+		if is_inside_tree(): get_tree().change_scene_to_packed(next_level if (next_level and ResourceLoader.exists(next_level.resource_path)) else load("res://Scenes/main_menu.tscn"))
